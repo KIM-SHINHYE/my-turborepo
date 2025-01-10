67a729a9d6d5d07096e9437e6179ffc9c0e42d3f
KIM-SHINHYE <kshinhye1102@gmail.com>
2025-01-10 10:12:36 +0900

    motion 추가

---
+++ apps/web/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { motion } from "motion/react";
+
+export default function Dashboard() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <motion.div
+        className="w-10 h-10 bg-red-500"
+        animate={{
+          scale: 2,
+          transition: { duration: 2 },
+        }}
+      />
+    </div>
+  );
+}
